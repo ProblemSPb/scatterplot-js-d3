375ee4db290f658743521e0855d613d8cf73e224
ProblemSPb <ussr83@mail.ru>
2021-10-23 10:20:29 +0200

    calibrated axis and bound them to the data set, generated Dots

--- script.js
+++ script.js
@@ -18,9 +18,23 @@ function drawCanvas () {
 
 function generateScales () {
     xScale = d3.scaleLinear()
+                .domain([d3.min(values, (item) => {
+                    return item['Year'];
+                // -1 to move the first tick to the right and does not let dots to overlap the border of chart
+                }) - 1, d3.max(values, (item) => {
+                    return item['Year'];
+                // +1 to move the first tick to the left and does not let dots to overlap the border of chart
+                }) + 1 ])
                 .range([padding, width - padding])
 
     yScale = d3.scaleTime()
+                .domain([d3.min(values, (item) => { // select the min time in sec and convert to date object
+                    // -10 seconds to move the min tick and does not let dots to overlap the border of chart
+                    return new Date ((item['Seconds'] - 10) * 1000);
+                }), d3.max(values, (item) => { // select the max time in sec and convert to date object
+                    // +10 seconds to move the min tick and does not let dots to overlap the border of chart
+                    return new Date ((item['Seconds'] +10 ) * 1000);
+                })])
                 .range([padding, height - padding])
 }
 
@@ -28,21 +42,34 @@ function drawDataPoints () {
     svg.selectAll('circle')
     .data(values)
     .enter()
-    // .append('circle')
-    // .attr('cx', (d, i) => d.year)
-    // .attr('cy', (d, i) => d.time)
-    // .attr('r', 5)
+    .append('circle')
+    .attr('class', 'dot')
+    .attr('data-xvalue', (item) => {
+        return item['Year'];
+    })
+    .attr('data-yvalue', (item) => {
+        return new Date(item['Seconds'] * 1000); // converting to miliseconds and creating a Date object
+    })
+    .attr('r', 5)
+    .attr('cx', (item) => {
+        return xScale(item['Year']);
+    })
+    .attr('cy', (item) => {
+        return yScale(new Date (item['Seconds'] * 1000));
+    })
 
 }
 
 function generateAxes () {
-    let xAxis = d3.axisBottom(xScale);
+    let xAxis = d3.axisBottom(xScale)
+                    .tickFormat(d3.format('d')); // removing coma in years formatting
     svg.append('g')
         .call(xAxis)
         .attr('id', 'x-axis')
         .attr('transform', 'translate(0, ' + (height - padding) + ')')
 
-    let yAxis = d3.axisLeft(yScale);
+    let yAxis = d3.axisLeft(yScale)
+                    .tickFormat(d3.timeFormat('%M:%S')); // show ticks in teh format Minutes:Seconds
     svg.append('g')
         .call(yAxis)
         .attr('id', 'y-axis')
